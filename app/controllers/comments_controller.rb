d1460c7925d8b97c5a30f2c35f1b42c285903d23
Maririn1223 <79556323+Maririn1223@users.noreply.github.com>
2021-05-16 13:10:31 +0900

    未ログイン状態でもプロトタイプ詳細が見れる

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,12 +1,11 @@
 class CommentsController < ApplicationController
 
   def create
-    @comment = Comment.new(comment_params)
-    if @comment.save
-      redirect_to prototype_path(@comment.prototype)
-      #@prototype = @comment.prototype
-      #@comments = @prototype.comments
-      #render "prototypes/show"
+    comment = Comment.new(comment_params)
+    if comment.save
+      redirect_to "/prototypes/#{comment.prototype.id}"
+    else
+      render "prototypes/show"
     end
   end
 
--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,10 +1,10 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :new, :create]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_prototype, except: [:index, :new, :create]
   before_action :move_to_index, except: [:index, :show]
 
   def index
-    @prototypes = Prototype.all.includes(:user)
+    @prototypes = Prototype.includes(:user)
   end
 
   def new
@@ -56,7 +56,7 @@ class PrototypesController < ApplicationController
 
     def move_to_index
       unless user_signed_in?
-        redirect_to action: :index
+      redirect_to action: :index
       end
     end
 end
